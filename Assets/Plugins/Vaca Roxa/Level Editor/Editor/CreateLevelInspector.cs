64e0fa5b870d3da2340ad60c1f4f11afb63ab20f
bakudas <bakudas@gmail.com>
2020-09-15 20:10:03 -0300

    level editor core works fine but not stable

---
+++ Assets/Plugins/Vaca Roxa/Level Editor/Editor/CreateLevelInspector.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Plugins.Vaca_Roxa.Level_Editor.Scripts;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CustomEditor(typeof(LevelConstructor))]
+public class CreateLevelInspector : Editor
+{
+    private Vector3 _mousePosition;
+    private LevelConstructor _level;
+    private bool _createdObjects = false;
+
+    public Vector3 GetMousePosition()
+    {
+        return _mousePosition;
+    }
+
+    private void StepPhysics()
+    {
+        Physics.autoSimulation = false;
+        
+        if (Physics.autoSimulation)
+            return; // do nothing if the automatic simulation is enabled
+
+        float timer = 0f;
+        timer += Time.deltaTime;
+
+        // Catch up with the game time.
+        // Advance the physics simulation in portions of Time.fixedDeltaTime
+        // Note that generally, we don't want to pass variable delta to Simulate as that leads to unstable results.
+        while (timer >= Time.fixedDeltaTime)
+        {
+            timer -= Time.fixedDeltaTime;
+            Physics.Simulate(Time.fixedDeltaTime);
+        }
+    }
+ 
+    [MenuItem("Tools/Scene Physics")]
+    private static void OpenWindow()
+    {
+        
+    }
+    
+    private void OnSceneGUI()
+    {
+        if (_level == null)
+        {
+            _level = (LevelConstructor) target;
+        }
+        
+        _mousePosition = Event.current.mousePosition;
+        Ray ray = HandleUtility.GUIPointToWorldRay(_mousePosition);
+        _mousePosition = ray.GetPoint(5f);
+
+        if (_createdObjects)
+        {
+            Event e = Event.current;
+            if (e.isMouse)
+            {
+                if (e.type == EventType.MouseUp)
+                {
+                    if (e.button == 0  && e.alt)
+                    {
+                        _level.CreateObject(GetMousePosition());
+                        EditorUtility.SetDirty(_level);
+                        EditorUtility.SetDirty(_level.gameObject);
+                    }
+                }
+                
+            }
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        if (GUILayout.Button("Liga a Física o/"))
+        {
+            StepPhysics();
+        }
+        
+        if (GUILayout.Button("Cria Level"))
+        {
+            _level.StartCreateLevel(_level.mapJson);
+        }
+        
+        if (GUILayout.Button("Cria Objeto com clique do Mouse = " + _createdObjects))
+        {
+            _createdObjects = !_createdObjects;
+        }
+
+        try
+        {
+            foreach (InGameObject gameObject in _level._createdObjects)
+            {
+                if (GUILayout.Button("Destruir o Objeto @" + gameObject.GetGameObject().transform.position))
+                {
+                    _level.DestroyGameObject(gameObject);
+                }
+            }
+
+        }
+        catch (Exception e)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Plugins/Vaca Roxa/Level Editor/Scripts/LevelConstructor.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+
+namespace Plugins.Vaca_Roxa.Level_Editor.Scripts
+{
+   [Serializable]
+    public class LevelConstructor : MonoBehaviour
+    {
+       [Header("Basic Level Setup")]
+        [SerializeField]
+        public string mapJson;
+        public List<InGameObject> _createdObjects;
+        
+        private List<GameObject> _CreatedObjectsAsGameObjects;
+        
+        [Header("Entities")]
+        [FormerlySerializedAs("Player")] public GameObject _player;
+        //TODO: INIMIGOS QUE VAI SER FODA
+        [FormerlySerializedAs("Bomb")] public GameObject _bomb;
+        [FormerlySerializedAs("Basic Floor")] public GameObject _basicFloor;
+
+        private Vector3 positions;
+
+        private JObject mapa;
+
+        private void Start()
+        {
+           
+           // get do tipo do tile
+           //tiletype = (JArray) mapa["tileTypes"];
+           
+           //StartCoroutine(CriaLevel(mapJson));
+           
+        }
+
+        public void StartCreateLevel(string map)
+        {
+           if (_createdObjects == null)
+           {
+              _createdObjects = new List<InGameObject>();
+           }
+
+           if (_CreatedObjectsAsGameObjects == null)
+           {
+              _CreatedObjectsAsGameObjects = new List<GameObject>();
+           }
+
+           var mapa = JObject.Parse(map);
+           var tiletype = (JArray)mapa["tileTypes"];
+           
+           for (int i = 0; i < tiletype.Count; i++)
+
+           {
+              positions.x = (float) mapa["positions"][i]["x"];
+              positions.y = (float) mapa["positions"][i]["y"];
+              positions.z = (float) mapa["positions"][i]["z"];
+              
+              GameObject go = (GameObject)Instantiate(_basicFloor, positions, Quaternion.identity);
+              
+              _createdObjects.Add( new InGameObject(go));
+              _CreatedObjectsAsGameObjects.Add(go);
+           }
+           
+        }
+
+        /*private IEnumerator CriaLevel(string json)
+        {
+           var mapa = JObject.Parse(json);
+           var tiletype = (JArray)mapa["tileTypes"];
+           
+           for (int i = 0; i < tiletype.Count; i++)
+
+           {
+              positions.x = (float) mapa["positions"][i]["x"];
+              positions.y = (float) mapa["positions"][i]["y"];
+              positions.z = (float) mapa["positions"][i]["z"];
+              
+              Instantiate(_basicFloor, positions, Quaternion.identity);
+              
+              //yield return new WaitForSeconds(.1f);
+           }
+           
+        }*/
+
+        public void CreateObject(Vector3 position)
+        {
+           if (_createdObjects == null)
+           {
+              _createdObjects = new List<InGameObject>();
+           }
+
+           if (_CreatedObjectsAsGameObjects == null)
+           {
+              _CreatedObjectsAsGameObjects = new List<GameObject>();
+           }
+
+           GameObject go = (GameObject) Instantiate(_basicFloor, position, Quaternion.Euler(0,0,0 ));
+           
+           _createdObjects.Add( new InGameObject(go));
+           _CreatedObjectsAsGameObjects.Add(go);
+
+        }
+
+        public void DestroyGameObject(InGameObject objectToDestroy)
+        {
+           _CreatedObjectsAsGameObjects.Remove(objectToDestroy.GetGameObject());
+           _createdObjects.Remove(objectToDestroy);
+           DestroyImmediate(objectToDestroy.GetGameObject());
+        }
+       
+    }
+    
+    [SerializeField]
+    public class InGameObject
+    {
+       public GameObject _GameObject;
+
+       public InGameObject(GameObject _obj)
+       {
+          _GameObject = _obj;
+       }
+
+       public GameObject GetGameObject()
+       {
+          return _GameObject;
+       }
+
+    }
+}
